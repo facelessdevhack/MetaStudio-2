ffd7f05449336a45da6065483fa320f24097a3ae
facelessdevhack <60548673+facelessdevhack@users.noreply.github.com>
2020-09-08 16:36:03 +0530

    Revert "update form"
    This reverts commit f704e9109e91e16edfeb924cb733fc94c9b9c4b1.

--- handler.php
+++ handler.php
@@ -1,31 +1,26 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+/*
+Tested working with PHP5.4 and above (including PHP 7 )
 
-    $to = "techedemy@gmail.com";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $csubject = $_REQUEST['subject'];
-    $cmessage = $_REQUEST['message'];
+ */
+require_once './vendor/autoload.php';
 
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+use FormGuide\Handlx\FormHandler;
 
-    $subject = "You have a message from your Industial Template.";
 
-    $logo = 'http://demos.pixelatethemes.com/industrial/images/header/logo-2.png';
+$pp = new FormHandler(); 
 
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Industrial Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'><a href='#'><img src='{$logo}' alt=''></a></td></tr></thead><tbody>";
-	$body .= "<tr><td style='border:none;'>{$name}</td><td style='border:none;'>Sender: {$from}</td></tr>";
-	$body .= "<tr><td style='border:none;'>Subject: {$csubject}</td></tr>";
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
+$validator = $pp->getValidator();
+$validator->fields(['name','email'])->areRequired()->maxLength(50);
+$validator->field('email')->isEmail();
+$validator->field('comments')->maxLength(6000);
 
-    $send = mail($to, $subject, $body, $headers);
 
-?>
+
+
+$pp->sendEmailTo('techedemy@gmail.com'); // ← Your email here
+
+echo $pp->process($_POST);
